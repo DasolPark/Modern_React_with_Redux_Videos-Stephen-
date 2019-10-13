3ad372c98ed4ceee4e6e64eb53ca407f5817c998
Dasol Park <dasol.park615@gmail.com>
2019-10-13 17:26:25 +0900

    111. Reminder on Event Handlers

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar />
       </div>
     );
