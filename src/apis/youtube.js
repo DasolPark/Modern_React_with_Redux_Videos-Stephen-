cd1bc89fad3778881dcc06b23c09f8c97d18afee
Dasol Park <dasol.park615@gmail.com>
2019-10-13 18:09:27 +0900

    115. Searching for Videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyDTXqr_0AeRITb904u-MUdBOlSTJGExy1g";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
